86e5218302d2c9a2c1b6344ad78f8a99b8ef096d
{hiroki} <{bigtreehrk2720@gmail.com}>
2023-02-13 19:12:03 +0900

    train header & footer

---
+++ src/TheFooter.vue
@@ -0,0 +1,12 @@
+<template>
+    <footer class = "fotter">
+        <small>&copy; todo</small>
+    </footer>
+</template>
+
+<style scoped>
+.footer { 
+    margin-top: 15px;
+    height: 30px;
+}
+</style>
